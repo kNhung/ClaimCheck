35e8d73a19aed10c766eb5cf1ed29952bd9df326
v1nk4n <vinhan188@gmail.com>
2025-11-30 10:30:36 +0700

    feat: using graph for verification

--- factchecker/modules/llm.py
+++ factchecker/modules/llm.py
@@ -3,6 +3,14 @@ import os
 import ollama
 import base64
 
+try:
+    from dotenv import load_dotenv  # type: ignore
+    load_dotenv()
+
+    print("Load env")
+except Exception:
+    pass
+
 DEFAULT_OLLAMA_MODEL = os.getenv("FACTCHECK_MODEL_NAME", "qwen3:4b")
 REASONING_MODEL = os.getenv("FACTCHECKER_REASONING_MODEL", None)  # Use faster model for reasoning if set
 JUDGE_MODEL = os.getenv("FACTCHECKER_JUDGE_MODEL", None)  # Use faster model for judging if set
